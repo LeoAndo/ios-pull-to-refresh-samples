3fad92e229d8044ad690528589f64691d25056e5
LeoAndo <leo.777.info@gmail.com>
2021-12-14 00:04:04 +0900

    add project.

---
+++ Sample01/Sample01/Sample01App.swift
@@ -0,0 +1,17 @@
+//
+//  Sample01App.swift
+//  Sample01
+//
+//  Created by LeoAndo on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct Sample01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
